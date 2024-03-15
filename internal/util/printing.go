f5c178e5b1d23ce2af4ef79d1d8b9121510d8dd6
taebinlim <taebinlim@taebinlimui-MacBookPro.local>
2024-03-15 13:03:51 +0900

    🌱 Update printing formatts each for restart and oom

--- internal/util/printing.go
+++ internal/util/printing.go
@@ -15,27 +15,39 @@ const (
 
 	// Sort by termination timestamp in ascending order.
 	SortFieldTerminationTime = "time"
+)
 
-	// When using the namespace provided by the `--namespace/-n` flag or current context.
-	// This represents: Pod, Container and Termination Time
-	singleNamespaceFormatting = "%s\t%s\t%s\n"
+var (
 
-	// When using the `all-namespaces` flag, we must show which namespace the pod was in, this becomes an extra column.
-	// This represents: Namespace, Pod, Container and Termination Time
-	allNamespacesFormatting = "%s\t%s\t%s\t%s\n"
+// When using the namespace provided by the `--namespace/-n` flag or current context.
+// This represents: Pod, Container and Termination Time
+// singleNamespaceFormatting = "%s\t%s\t%s\n"
+// When using the `all-namespaces` flag, we must show which namespace the pod was in, this becomes an extra column.
+// This represents: Namespace, Pod, Container and Termination Time
+// allNamespacesFormatting = "%s\t%s\t%s\t%s\n"
 )
 
 var t = tabwriter.NewWriter(os.Stdout, 10, 1, 5, ' ', 0)
 
-func PrintPodsInfoInAllNameSpace(pods kube.Pods, noHeaders bool) (err error) {
+func PrintPodsInfoInAllNameSpace(pods kube.Pods, noHeaders bool, filterType string) (err error) {
+	var allNamespacesFormatting string = "%s\t%s\t%s\t%s\n"
 	if !noHeaders {
-		_, err := fmt.Fprintf(t, allNamespacesFormatting, "NAMESPACE", "POD", "TERMINATION TIME", "RESTARTS")
+		if filterType == "restart" {
+			allNamespacesFormatting = "%s\t%s\t%s\t%s\t%s\n"
+			_, err = fmt.Fprintf(t, allNamespacesFormatting, "NAMESPACE", "POD", "TERMINATION TIME", "RESTARTS", "EXIT CODE")
+		} else {
+			_, err = fmt.Fprintf(t, allNamespacesFormatting, "POD", "TERMINATION TIME", "RESTARTS")
+		}
 		if err != nil {
 			return err
 		}
 	}
 	for _, p := range pods {
-		_, err := fmt.Fprintf(t, allNamespacesFormatting, p.Pod.Namespace, p.Pod.Name, p.TerminatedTime, p.RestartCount)
+		if filterType == "restart" {
+			_, err = fmt.Fprintf(t, allNamespacesFormatting, p.Pod.Namespace, p.Pod.Name, p.TerminatedTime, p.RestartCount, p.ExitCode)
+		} else {
+			_, err = fmt.Fprintf(t, allNamespacesFormatting, p.Pod.Name, p.TerminatedTime, p.RestartCount)
+		}
 		if err != nil {
 			return err
 		}
@@ -44,16 +56,26 @@ func PrintPodsInfoInAllNameSpace(pods kube.Pods, noHeaders bool) (err error) {
 	return nil
 }
 
-func PrintPodsInfoInSingleNameSpace(pods kube.Pods, noHeaders bool) (err error) {
+func PrintPodsInfoInSingleNameSpace(pods kube.Pods, noHeaders bool, filterType string) (err error) {
+	var singleNamespaceFormatting string = "%s\t%s\t%s\n"
 	if !noHeaders {
-		_, err := fmt.Fprintf(t, singleNamespaceFormatting, "POD", "TERMINATION TIME", "RESTARTS")
+		if filterType == "restart" {
+			singleNamespaceFormatting = "%s\t%s\t%s\t%s\n"
+			_, err = fmt.Fprintf(t, singleNamespaceFormatting, "POD", "TERMINATION TIME", "RESTARTS", "EXIT CODE")
+		} else {
+			_, err = fmt.Fprintf(t, singleNamespaceFormatting, "POD", "TERMINATION TIME", "RESTARTS")
+		}
 		if err != nil {
 			return err
 		}
 	}
 	for _, p := range pods {
+		if filterType == "restart" {
+			_, err = fmt.Fprintf(t, singleNamespaceFormatting, p.Pod.Name, p.TerminatedTime, p.RestartCount, p.ExitCode)
+		} else {
+			_, err = fmt.Fprintf(t, singleNamespaceFormatting, p.Pod.Name, p.TerminatedTime, p.RestartCount)
+		}
 
-		_, err := fmt.Fprintf(t, singleNamespaceFormatting, p.Pod.Name, p.TerminatedTime, p.RestartCount)
 		if err != nil {
 			return err
 		}
